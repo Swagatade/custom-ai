972c2c3974dde6515cf3864a85e2cc19ea3016d4
Swagata Dey <116741845+Swagatade@users.noreply.github.com>
2025-02-17 20:07:12 +0530

    Delete duckduckgo_patch.py

--- duckduckgo_patch.py
+++
@@ -1,8 +0,0 @@
-from duckduckgo_search import Client as DDGSClient
-_orig_init = DDGSClient.__init__
-
-def _patched_init(self, *args, **kwargs):
-    kwargs.pop('proxies', None)
-    _orig_init(self, *args, **kwargs)
-
-DDGSClient.__init__ = _patched_init
